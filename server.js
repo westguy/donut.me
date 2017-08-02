e61c0d30a9ba5fc03d579e3fb18a20d6e68da860
Tristan West <wtris@sas.upenn.edu>
2017-08-02 15:54:47 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var app = express();
+var https = require("https");
+var fs = require("fs");
+var index = fs.readFileSync("index.html");
+
+app.use(express.static("."));
+app.listen(8080,function(){
+    console.log("Server Running…");
+});
+
+app.get("/", function(req, res){
+    res.send(index);
+});
+
+app.get("/donuts", function(req, res){
+    var token = "Bearer tFAr5YtkP-r7ZPR5Aj6-Ck5gfKXFKoDOrqjZ3-nuHVFppkMf0D_PxnEibe8bdNdKIx8WN7wa_osnRj4Yjc-D5SkkgSw9hadK7oAkQK2fFBEYLqjEh8T9WXSqV4p7WXYx";
+    var options = {
+        hostname: "api.yelp.com",
+        path: "/v3/businesses/search?term=donuts&location=19104",
+        method: "GET",
+        headers: {
+            "Authorization": token
+        }
+    };
+
+    var req = https.request(options, function(response) {
+        var data = "";
+        console.log("Status: " + response.statusCode);
+        console.log("Headers: " + JSON.stringify(response.headers));
+        response.setEncoding("utf8");
+        response.on("data", function (body) {
+            data += body;
+        });
+
+        response.on("end", function(){
+            res.end(data);
+        });
+    }).end();
+});
+
+
